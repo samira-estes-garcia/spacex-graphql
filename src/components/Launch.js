6458453f7d2568da2c25bf11ff2f16040783c478
samira-estes-garcia <samira.estes.garcia@gmail.com>
2021-02-06 12:39:51 -0600

    added 3 images to each card, added default card if no images found

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import spacex from '../spacex.jpeg'
 
 export const Launch = ({launchPast}) => {
+
+    // display first 3 flickr images if array not empty, if array is empty, display default spacex image
+    let image;
+    if  (launchPast.links.flickr_images.length > 0) {
+        image = launchPast.links.flickr_images
+        .slice(0,3)
+        .map(image => (<img className="img" key={`${launchPast.mission_name}-${image}`} src={image} alt="{launchPast.mission_name}" />))
+    } else {
+        image = <img className="alt-img" src={spacex} alt="Placeholder" />
+    }
+
     return (
         <div className="launch">
+            <div className="launch-img">
+                {image}
+            </div>
             <div className="launch-name">
                 <p>{launchPast.mission_name}</p>
             </div>
